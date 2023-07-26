a99bc64b5d92dd33a60a2ae587f2c8a5db785a85
Cameron Asselin <100627085+CameronAsselin@users.noreply.github.com>
2023-07-26 12:46:27 -0400

    07-26-23
    -Added nvim config
    -Changed font to Mononoke
    -Changed colors to Catpuccin Macchiato

--- config/dunst/scripts/volume_brightness.sh
+++ config/dunst/scripts/volume_brightness.sh
@@ -22,14 +22,14 @@ function get_brightness {
 function get_volume_icon {
     volume=`get_volume`
     mute=`get_mute`
-    if [ $volume -eq 0 ] && [ $mute == "yes" ] ; then
+    if [ $mute == "yes" ] ; then
         volume_icon="󰝟"
     elif [ $volume -lt 35 ] ; then
-	volume_icon=""
+	volume_icon="󰕿"
     elif [ $volume -lt 70 ]; then
-        volume_icon=""
+        volume_icon="󰖀"
     else
-        volume_icon=""
+        volume_icon="󰕾"
     fi
 }
 
---
+++ config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,7 @@
+---@type ChadrcConfig 
+ local M = {}
+ M.ui = {
+  theme = 'catppuccin',
+  transparency = true,
+}
+ return M
